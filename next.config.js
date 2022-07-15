f035bd1294d08465f88a00d296f90fa7bbf9e44a
Zen Farhat <zeinfarhat6@gmail.com>
2022-07-15 22:21:09 +1000

    removed webpack bug

--- next.config.js
+++ next.config.js
@@ -4,18 +4,6 @@ const nextConfig = {
   images: {
     domains: ["frontendsimplified.com"],
   },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      issuer: {
-        test: /\.(js|ts)x?$/,
-      },
-
-      use: ["@svgr/webpack"],
-    })
-
-    return config
-  },
 }
 
 module.exports = nextConfig
